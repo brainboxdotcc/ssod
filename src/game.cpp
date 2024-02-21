9aa3993e695fb8eda6aca367ef012d68cb1be0c0
Craig Edwards <craigedwards@brainbox.cc>
2024-02-21 15:42:58 +0000

    copy over last resurrect

--- src/game.cpp
+++ src/game.cpp
@@ -380,6 +380,7 @@ void game_nav(const dpp::button_click_t& event) {
 		new_p.gold = p.gold;
 		new_p.silver = p.silver;
 		new_p.gotfrom = p.gotfrom;
+		new_p.last_resurrect = p.last_resurrect;
 		new_p.death_xp_loss();
 		new_p.reset_to_spawn_point();
 		update_live_player(event, new_p);
