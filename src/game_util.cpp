d576414b6bfee1ba1df009309610b7319974cf84
Craig Edwards <craigedwards@brainbox.cc>
2024-03-13 08:33:06 +0000

    better inventory descriptions

--- src/game_util.cpp
+++ src/game_util.cpp
@@ -48,32 +48,26 @@ sale_info get_sale_info(const std::string& name) {
 
 std::string describe_item(const std::string& modifier_flags, const std::string& name) {
 	auto res = db::query("SELECT idesc FROM game_item_descs WHERE name = ?", {name});
-	std::string rv{res.size() ? res[0].at("idesc") : ""};
-	if (!rv.empty()) {
-		return rv;
-	}
-	
-	if (modifier_flags.empty()) {
-		return name;
-	}
+	std::string rv{res.size() ? res[0].at("idesc") : name};
+
 	if (modifier_flags.substr(0, 3) == "ST+") {
-		return fmt::format("Adds {} stamina when used", modifier_flags.substr(3));
+		return fmt::format("Stamina **+{}**: {}", modifier_flags.substr(3), rv);
 	} else if (modifier_flags.substr(0, 3) == "SK+") {
-		return fmt::format("Adds {} skill when used", modifier_flags.substr(3));
+		return fmt::format("Skill **+{}**: {}", modifier_flags.substr(3), rv);
 	} else if (modifier_flags.substr(0, 3) == "LK+") {
-		return fmt::format("Adds {} luck when used", modifier_flags.substr(3));
+		return fmt::format("Luck **+{}**: {}", modifier_flags.substr(3), rv);
 	} else if (modifier_flags.substr(0, 3) == "SN+") {
-		return fmt::format("Adds {} sneak when used", modifier_flags.substr(3));
+		return fmt::format("Sneak **+{}**: {}", modifier_flags.substr(3), rv);
 	} else if (modifier_flags.substr(0, 2) == "W+") {
-		return fmt::format("Adds {} to your weapon score", modifier_flags.substr(2));
+		return fmt::format("Weapon **+{}**: {}", modifier_flags.substr(2), rv);
 	} else if (modifier_flags.substr(0, 2) == "A+") {
-		return fmt::format("Adds {} to your armour score", modifier_flags.substr(2));
-	} else if (modifier_flags[0] == 'W') {
-		return fmt::format("Weapon, rating {}",modifier_flags.substr(1));
-	} else if (modifier_flags[0] == 'A') {
-		return fmt::format("Armour, rating {}",modifier_flags.substr(1));
+		return fmt::format("Armour **+{}**: {}", modifier_flags.substr(2), rv);
+	} else if (!modifier_flags.empty() && modifier_flags[0] == 'W') {
+		return fmt::format("Weapon **{}**: {}",modifier_flags.substr(1), rv);
+	} else if (!modifier_flags.empty() && modifier_flags[0] == 'A') {
+		return fmt::format("Armour **{}**: {}",modifier_flags.substr(1), rv);
 	}
-	return name;
+	return rv;
 }
 
 void premium_required(const dpp::interaction_create_t& event) {
